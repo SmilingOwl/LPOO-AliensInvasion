23d59dbc0f9f6e65c890ce9c770bec84efd239d3
Juliana Marques <julianamcm05@gmail.com>
2018-04-24 17:04:33 +0100

    Inicialização

---
+++ core/src/com/mygdx/game/Gui/StartMenu.java
@@ -0,0 +1,64 @@
+package com.mygdx.game.Gui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class StartMenu implements Screen {
+
+
+    private Image playButton;
+    private Image exitButton;
+    private Image scoresButton;
+    private Texture backgroundMenu;
+
+    public StartMenu(){
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/mygdx/game/Logic/Hero.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.Logic;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Hero extends Sprite {
+
+    protected enum State {Running, Jumping, Standing, Armed, Dead};
+
+    private Animation runningAnimation;
+    private Animation standingAnimation;
+    private Animation jumpingAnimation;
+    private Animation deadAnimation;
+    private Animation armedAnimation;
+
+}
